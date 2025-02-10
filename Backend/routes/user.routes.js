7de3cb39636521f44c9b07c089067d93b9b02e1e
Sambhav <sambhavpandey1@gmail.com>
2025-02-10 18:46:44 +0545

    User Registration Finished

---
+++ Backend/routes/user.routes.js
@@ -0,0 +1,16 @@
+const express = require ("express");
+const { body } = require ("express-validator");
+const userController = require("../controllers/user.controllers")
+
+const router = express.Router()
+
+router.post('/register',[
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('fullname.firstname').isLength({min:3}).withMessage('First Name must be at least 3 Characters Long'),
+    body('password').isLength({min:6}).withMessage('Password must be at least 6 Characters Long')
+],
+
+
+userController.registerUser)
+
+module.exports = router
